25e7cb0ba80123e28e78243420808a4f2e9e4b28
Bhawesh <bhaweshjainskype@gmail.com>
2024-01-12 14:37:30 +0530

    OnBoardingPage Complete

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
@@ -1,3 +1,28 @@
 package com.loc.newsapp.presentation.onboarding
 
-data class page()
+import androidx.annotation.DrawableRes
+import com.loc.newsapp.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int,
+)
+
+val pages = listOf(
+    Page(
+        title = "This is the Page 1 of boarding",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding1
+    ),
+    Page(
+        title = "This is the Page 2 of boarding",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding2
+    ),
+    Page(
+        title = "This is the Page 3 of boarding",
+        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        image = R.drawable.onboarding3
+    )
+)
